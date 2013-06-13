b582fccd2f93d1889f4440a1d6449e6320b7067f
Roman Zimmermann <roman@more-onion.com>
2013-06-13 15:18:57 +0200

    Little helper for field_config (aka field_settings).

---
+++ lib/Drupal/little_helpers/Field/Field.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\little_helpers\Field;
+
+class Field {
+  protected $id = NULL;
+  public $entity_types = array();
+  public $field_name;
+  public $type;
+  public $module;
+  public $active = 1;
+  public $storage = array();
+  public $locked = FALSE;
+  public $cardinality = 1;
+  public $translatable = FALSE;
+  public $deleted = 0;
+  public $settings = array();
+
+  public static function byName($name) {
+    $class = \get_called_class();
+    return new $class(\field_read_field($name));
+  }
+
+  public static function fromType($type) {
+    $class = \get_called_class();
+    $new = new $class(array());
+    $new->setType($type);
+    return $new;
+  }
+
+  public function __construct($data) {
+    foreach ($data as $k => $v) {
+      $this->$k = $v;
+    }
+  }
+
+  /**
+   * Load default data for this field-type.
+   * 
+   * @see \field_create_field().
+   */
+  public function setType($type) {
+    $field_type = \field_info_field_types($type);
+    $this->settings += \field_info_field_settings($type);
+    $this->module = $field_type['module'];
+    $this->type = $type;
+  }
+
+  /**
+   * Save field configuration to database.
+   */
+  public function save() {
+    if ($this->data['id']) {
+      \field_update_field((array) $this);
+    } else {
+      $this->data = \field_create_field((array) $this);
+    }
+  }
+}
