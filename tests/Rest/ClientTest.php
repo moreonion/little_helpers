34f925fbd036b18bea1d18bd988afd745bb0f5d8
Roman Zimmermann <roman@more-onion.com>
2020-03-20 14:56:35 +0100

    Add simple test for the REST client.

---
+++ tests/Rest/ClientTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\little_helpers\Rest;
+
+use Upal\DrupalUnitTestCase;
+
+/**
+ * Test the JSON/REST API-client.
+ */
+class ClientTest extends DrupalUnitTestCase {
+
+  /**
+   * Create a client instance with a stubbed sendRequest() method.
+   */
+  protected function mockClient(string $endpoint = 'https://example.com', array $options = []) {
+    return $this->getMockBuilder(Client::class)
+      ->setConstructorArgs([$endpoint, $options])
+      ->setMethods(['sendRequest'])
+      ->getMock();
+  }
+
+  /**
+   * Test that send() adds slashes automatically as needed.
+   */
+  public function testAddSlash() {
+    $client = $this->mockClient();
+    $client->expects($this->once())->method('sendRequest')
+      ->with('https://example.com/no-slash', $this->anything())
+      ->willReturn((object) ['data' => '{}']);
+    $client->get('no-slash');
+  }
+
+}
